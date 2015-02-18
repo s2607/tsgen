326ccbf90c1716d8f3ef103efa725f37083ae2f2
phreaker2600 <swwiley@gmail.com>
2015-02-18 14:43:31 +0000

    gets input

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
 import "fmt"
+import "bufio"
+import "os"
 
 func main() {
 
 	fmt.Printf("hello world")
+	reader := bufio.NewReader(os.Stdin)
+	text, e := reader.ReadString('\n')
+	if e == nil {
+		fmt.Println(text)
+	} else {
+		fmt.Println(e)
+	}
 }
