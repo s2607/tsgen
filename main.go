bb03919f018928b525ea970a535deea470dd224a
phreaker2600 <swwiley@gmail.com>
2015-02-18 15:32:38 +0000

    doesnt split quit right

--- main.go
+++ main.go
@@ -3,15 +3,41 @@ package main
 import "fmt"
 import "bufio"
 import "os"
+import "strings"
+import "strconv"
+
+type class struct {
+	year    int
+	subject string
+	grade   rune
+	book    string
+}
+
+func (this class) String() string {
+	return fmt.Sprintf("%d\t%s\t%c\t%s", this.year, this.subject, this.grade, this.book)
+}
+func getClass(in *bufio.Reader) *class {
+	Class := new(class)
+	//TODO: do this correctly
+	line, e := in.ReadString('\n')
+	if e == nil {
+		feilds := strings.Split(line, "\t")
+		i, e := strconv.ParseInt(feilds[0], 0, 32)
+		Class.year = int(i)
+		Class.subject = feilds[1]
+		Class.grade = rune(feilds[2][0])
+		Class.book = feilds[3]
+		if e == nil {
+			return Class
+		}
+	}
+	return nil
+
+}
 
 func main() {
 
-	fmt.Printf("hello world")
 	reader := bufio.NewReader(os.Stdin)
-	text, e := reader.ReadString('\n')
-	if e == nil {
-		fmt.Println(text)
-	} else {
-		fmt.Println(e)
-	}
+	firstclass := getClass(reader)
+	fmt.Println(firstclass)
 }
